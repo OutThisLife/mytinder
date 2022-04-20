3d52c63e52cbd6c65c0a73b0f56e3852f7ad8e6f
Talasan Nicholson <talasan.nicholson@gmail.com>
2022-04-20 14:44:25 -0500

    chore: uptick

--- src/lib/fb.ts
+++ src/lib/fb.ts
@@ -13,6 +13,8 @@ export default async (): Promise<string> => {
           `https://www.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token`
         )
 
+        console.log('[INFO]', page.url())
+
         await page.click('#email')
         await page.keyboard.type(`${process.env.FB_EMAIL}`)
 
@@ -23,6 +25,8 @@ export default async (): Promise<string> => {
         await page.waitForNavigation()
 
         const url = new URL(page.url())
+
+        console.log('[INFO]', url.toString())
 
         if (!url.pathname.endsWith('login_success.html')) {
           throw new Error('Login failed')
